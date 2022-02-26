345dfa4f801d608f651ce534931ef7259439be41
MerttErturkk <35269315+MerttErturkk@users.noreply.github.com>
2022-02-27 01:18:31 +0300

    re-arrangement

--- Graduation_Project_workspace/inspect_features.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv("features_30_sec.csv")
-
-
-print(str(df.head()) + "\n***************************************************************")
-print(str(df.info()) + "\n***************************************************************")
-pd.set_option('display.max_columns', 60)
-print(df.describe())    
